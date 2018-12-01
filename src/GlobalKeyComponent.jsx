bdb479b285004b9da778632e878140b770e7d4b9
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-12-01 10:20:18 -0800

    Add initial file

---
+++ src/GlobalKeyComponent.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+const unusedKeys = new Set();
+const elementMap = {};
+function getElementFragment(key) {
+
+    let frag = elementMap[key];
+    if (!frag) {
+
+        frag = document.createDocumentFragment();
+        elementMap[key] = frag;
+
+    }
+
+    unusedKeys.delete(key);
+    return frag;
+
+}
+
+function markKeyUnused(key) {
+
+    unusedKeys.add(key);
+
+}
+
+function cleanCache() {
+
+    for (const key in unusedKeys) {
+
+        delete elementMap[key];
+
+    }
+
+}
+
+export class GlobalKeyComponent extends Component {
+
+    static cleanCache() {
+
+        cleanCache();
+
+    }
+
+    /* Life Cycle Functions */
+    render() {
+
+        const variant = this.props.variant || 'div';
+        const globalKey = this.props.globalKey || null;
+
+        if (!globalKey) {
+            console.warn('GlobalKeyComponent: A globalKey must be provided.');
+        }
+
+        return (<variant ref = { el => this._container = el }></variant>);
+
+    }
+
+    componentDidMount() {
+
+        this._fragment = getElementFragment(this.props.globalKey);
+        this._container.appendChild(this._fragment);
+
+    }
+
+    componentDidUpdate() {
+
+        render(this.renderContents(), this._fragment);
+
+    }
+
+    componentWillUnmount() {
+
+        markKeyUnused(this.props.globalKey);
+
+    }
+
+    /* Interface */
+    renderContents() { }
+
+}
